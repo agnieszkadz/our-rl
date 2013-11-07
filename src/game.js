d9ba3d58781b2dfb3ff468451c3b59218e005eb5
agnieszkadz <agnieszka.dzierbunowicz@gmail.com>
2013-11-07 20:13:41 +0100

    map with working following player accordingly to viewports.

--- src/game.js
+++ src/game.js
@@ -2,31 +2,40 @@
 var wall='#d0c5ad';  //x
 var floor='#704b41';  //f
 var water='#5d71a0';   //w
-var enemy='#c60411';  //e
+var enemy='#c60411';  //e not used
 var treasure='#FFCC11';  //t
-
+var przesuniecieX = 0,
+    przesuniecieY = 0,
+    oknoEkranuWidth = 15,
+    oknoEkranuHeight = 15;
+	var canvas, context;
+	
+	
 var map = [
     ['x', 'f', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-    ['x', 'f', 'f', 'e', 'f', 'f', 'f', 'f', 'e', 'x', 'x', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 't', 'x'],
-    ['x', 'f', 'f', 'f', 'x', 'f', 'f', 'e', 'f', 'x', 'x', 'f', 'x', 'f', 'f', 'x', 'f', 'e', 'f', 'x'],
+    ['x', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'x', 'x', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 't', 'x'],
+    ['x', 'f', 'f', 'f', 'x', 'f', 'f', 'f', 'f', 'x', 'x', 'f', 'x', 'f', 'f', 'x', 'f', 'f', 'f', 'x'],
     ['x', 'f', 'f', 'x', 'x', 'f', 'x', 'x', 'f', 'x', 'x', 'f', 'x', 'f', 'f', 'x', 'x', 'f', 'f', 'x'],
-    ['x', 'e', 'f', 'f', 'f', 'f', 'f', 'x', 'f', 'x', 'x', 'f', 'x', 'w', 'w', 'w', 'x', 'f', 'f', 'x'],
-    ['x', 'w', 'w', 'f', 'f', 'f', 'e', 'f', 'f', 'x', 'x', 'f', 'x', 'w', 'e', 'w', 'x', 'f', 'f', 'x'],
+    ['x', 'f', 'f', 'f', 'f', 'f', 'f', 'x', 'f', 'x', 'x', 'f', 'x', 'w', 'w', 'w', 'x', 'f', 'f', 'x'],
+    ['x', 'w', 'w', 'f', 'f', 'f', 'f', 'f', 'f', 'x', 'x', 'f', 'x', 'w', 'f', 'w', 'x', 'f', 'f', 'x'],
     ['x', 'w', 'f', 'f', 'f', 'x', 'x', 'x', 'f', 'x', 'x', 'f', 'x', 'f', 't', 'x', 'x', 'f', 'f', 'x'],
     ['x', 'w', 'f', 'x', 'f', 'f', 'f', 'x', 'f', 'x', 'x', 'f', 'x', 'x', 'x', 'x', 'f', 'f', 'f', 'x'],
     ['x', 'f', 'f', 'x', 'f', 'f', 'f', 'x', 'f', 'x', 'x', 'f', 'x', 'x', 'f', 'f', 'f', 'f', 'f', 'x'],
-    ['x', 'e', 'f', 'x', 'f', 'f', 'f', 'x', 'f', 'f', 'f', 'f', 'x', 'x', 'f', 'f', 'f', 'f', 'f', 'x'],
+    ['x', 'f', 'f', 'x', 'f', 'f', 'f', 'x', 'f', 'f', 'f', 'f', 'x', 'x', 'f', 'f', 'f', 'f', 'f', 'x'],
 	['x', 't', 'x', 'x', 'x', 'x', 'x', 'x', 'f', 'f', 'f', 'f', 'x', 'x', 'f', 'x', 'f', 'f', 'f', 'x'],
     ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'f', 'f', 'x', 'x', 'f', 'x', 'x', 'f', 'x', 'f', 'f', 'f', 'x'],
-    ['x', 'x', 'x', 'f', 'f', 'f', 'f', 'f', 'f', 'x', 'e', 'f', 'f', 'f', 'f', 'x', 'f', 'f', 'f', 'x'],
+    ['x', 'x', 'x', 'f', 'f', 'f', 'f', 'f', 'f', 'x', 'f', 'f', 'f', 'f', 'f', 'x', 'f', 'f', 'f', 'x'],
     ['x', 'x', 'x', 'w', 'w', 'w', 'w', 'w', 'x', 'x', 'f', 'x', 'f', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
     ['x', 'x', 'x', 'w', 'w', 'w', 'w', 'w', 'x', 'x', 'f', 'x', 'f', 'x', 'w', 'w', 'w', 'w', 'w', 'x'],
-    ['x', 'f', 'f', 'f', 'w', 'e', 'w', 'w', 'f', 'x', 'f', 'x', 'f', 'x', 'f', 'x', 'x', 'x', 'w', 'x'],
+    ['x', 'f', 'f', 'f', 'w', 'f', 'w', 'w', 'f', 'x', 'f', 'x', 'f', 'x', 'f', 'x', 'x', 'x', 'w', 'x'],
     ['x', 'f', 'f', 'f', 'w', 'w', 'w', 'w', 'f', 'x', 't', 'x', 'f', 'x', 'f', 'x', 'w', 'w', 'w', 'x'],
     ['x', 'f', 'f', 'x', 'x', 'x', 'f', 'f', 'f', 'x', 'f', 'x', 'f', 'x', 'f', 'x', 'w', 'x', 'x', 'x'],
-    ['x', 'f', 'f', 't', 'x', 'x', 'f', 'f', 'f', 'f', 'e', 'x', 'e', 'f', 'f', 'x', 'w', 'w', 'w', 'x'],
+    ['x', 'f', 'f', 't', 'x', 'x', 'f', 'f', 'f', 'f', 'f', 'x', 'f', 'f', 'f', 'x', 'w', 'w', 'w', 'x'],
 	['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'w', 'x']
 ];
+
+var mapIloscKafelekWidth = map[0].length,
+    mapIloscKafelekHeight = map[0].length;
 
 // player's position
 var player = {
@@ -39,17 +48,21 @@ function run() {
     var gap = 2;
 
     // rectangles size
-    var size = 10;
+    var rectanglesSize = 10;
 
     // getting canvas
-    var c = document.getElementById("gameCanvas");
-
+    canvas = document.getElementById("gameCanvas");
+    context = canvas.getContext("2d"); // !
+	    canvas.focus(); // !
     // adding event listeners to move player
     // http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
     window.addEventListener('keydown', this.check, false);
+	
+	canvas.width = 250;
+    canvas.height = 250;
 
     // to redraw in loop 
-    window.requestAnimFrame = (function () {
+   window.requestAnimFrame = (function () {
         return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
             window.setTimeout(callback, 1000 / 80);
         };
@@ -57,25 +70,39 @@ function run() {
 
     // main loop
     (function animloop() {
-        requestAnimFrame(animloop);
+        
 
         // clear canvas
-        var ctx = c.getContext("2d");
-        ctx.clearRect(0, 0, c.width, c.height);
+        var context = canvas.getContext("2d");
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
-        draw_map(c, map, size, 2);
+        draw_map(canvas, map, rectanglesSize, 2, context);
+		requestAnimFrame(animloop);
         //console.log('draw');
     })();
+	//animloop();
 
 }
 
 // draw map, players, monsters, etc.
 
-function draw_map(c, map, size, gap) {
+function draw_map(canvas, map, rectanglesSize, gap, context) {
     // draw map
+	 context.clearRect(0,0,canvas.width, canvas.height);
     var color;
-    for (var i = 0; i < map[0].length; ++i) {
-        for (var j = 0; j < map[0].length; ++j) {
+	
+	przesuniecieX = Math.floor(player.x - Math.floor(0.5 * oknoEkranuWidth));
+        if (przesuniecieX < 0) przesuniecieX = 0;
+        if (przesuniecieX+oknoEkranuWidth > mapIloscKafelekWidth) przesuniecieX = mapIloscKafelekWidth - oknoEkranuWidth;
+        
+        
+        przesuniecieY = Math.floor(player.y - Math.floor(0.5 * oknoEkranuHeight));
+        if (przesuniecieY < 0) przesuniecieY = 0;
+        if (przesuniecieY+oknoEkranuHeight > mapIloscKafelekHeight) przesuniecieY = mapIloscKafelekHeight - oknoEkranuHeight;
+        
+	
+    for (var j = 0 + przesuniecieY; j < oknoEkranuHeight + przesuniecieY; ++j) {
+        for (var i = 0 + przesuniecieX; i < oknoEkranuWidth + przesuniecieX; ++i) {
             if (map[i][j] === 'x') {
                 color = wall;
             } else if (map[i][j] === 'w') {
@@ -87,18 +114,31 @@ function draw_map(c, map, size, gap) {
             } else if (map[i][j] === 't') {
                 color = treasure;
             }
-            var ctx = c.getContext("2d");
-            ctx.fillStyle = color;
-            ctx.fillRect((gap + size) * i, (gap + size) * j, size, size);
+   //         var ctx = canvas.getContext("2d");
+            context.fillStyle = color;
+            context.fillRect((gap + rectanglesSize) * (i-przesuniecieX), (gap + rectanglesSize) * (j-przesuniecieY), rectanglesSize, rectanglesSize);
         }
     }
 
     // draw player
-    var ctx = c.getContext("2d");
-    ctx.fillStyle = '#00FF00';
-    ctx.fillRect((gap + size) * player.x, (gap + size) * player.y, size, size);
+//var ctx = canvas.getContext("2d");
+    context.fillStyle = '#00FF00';
+    context.fillRect((gap + rectanglesSize) * (player.x-przesuniecieX), (gap + rectanglesSize) * (player.y-przesuniecieY), rectanglesSize, rectanglesSize);
 }
-
+/*
+function draw() {
+        context.clearRect(0,0,canvas.width, canvas.height);
+        for (y = 0; y <= oknoEkranuHeight; y++) {
+            for (x = 0; x <= oknoEkranuWidth; x++) {
+                theX = x * 32;
+                theY = y * 32;
+                context.drawImage(tiles, theX, theY, 32, 32);
+            }
+        }
+        context.fillStyle = 'red';
+        context.fillRect((player.x-przesuniecieX)*32, (player.y-przesuniecieY)*32, 32, 32);
+    }
+});*/
 // keys and collision
 
 function check(e) {
@@ -128,4 +168,8 @@ function check(e) {
         }
         player.y = player.y + 1;
     }
+	
+	
+        
+      //  draw();
 }
